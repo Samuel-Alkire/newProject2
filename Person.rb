351c9958840678e1793b2cfd127d1e416a59fdd4
Samuel Alkire <samuelatwo@gmail.com>
2022-06-12 14:49:02 -0400

    added proffesion instance variable

--- Person.rb
+++ Person.rb
@@ -1,16 +1,17 @@
 class Person
-    def initialize(name, age)
+    def initialize(name, age, prof)
         @name= name
         @age= age
+        @prof=prof
     end
     def info()
-        puts "Your name is #{@name} and you are #{@age} years old"
+        puts "Your name is #{@name} and you are #{@age} years old and work as a #{@prof}"
     end
     def run(miles)
         puts "you ran #{miles} miles"
     end
 end
 
-bob= Person.new("Bob", 18)
+bob= Person.new("Bob", 18, "farmer")
 bob.info
 bob.run(8)
